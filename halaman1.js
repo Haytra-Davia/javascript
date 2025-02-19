e7093ed0fd967e11bde3cb7a4fa011acb5250967
Haytra-Davia <asaduasyuja@gmail.com>
2025-02-19 20:51:48 +0700

    js

---
+++ halaman1.js
@@ -0,0 +1,32 @@
+//variable let
+let  nama ="haytra"
+console.log(nama);
+//variable var
+var nami ="dibey"
+console.log(nami);
+//kelakuan let
+let namabelakang ="hallo"
+{
+    let namabelakang ="jaja"
+    console.log(namabelakang);
+}
+console.log(namabelakang);
+//kelakuan var
+var ip="klop"
+{
+    var lp="samsung"
+    console.log(lp);
+}
+console.log(ip);
+//kasus khusus
+jaja ="hako"
+{
+    jaja ="klp"
+    console.log(jaja);
+}
+console.log(jaja);
+//cost intinya dia jalabn menggunakn huruf kapital semua
+const UMUR="haui"
+console.log(UMUR);
+//tipe data
+
---
+++ variable/variable.js
@@ -0,0 +1,8 @@
+let data='saya',umur =17, alamat ='bekasi';
+ document.write(data);
+
+
+
+const kelas='satu';
+// kelas='dua';
+document.write(kelas)
