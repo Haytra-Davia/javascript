64d7bb03fe5ebe1bc739046c2821eba2cd48db1b
Haytra-Davia <asaduasyuja@gmail.com>
2025-04-01 17:48:08 +0700

    js

---
+++ aray/arat.js
@@ -0,0 +1,12 @@
+let nama = ["hadi", "bayu"];
+// menambahkan array
+nama.push("saputra");
+nama.push("haytra")
+// mendapatkan data panjang array
+ document.writeln(nama.length)
+//  mengubah array
+nama[2] = "saputra123"
+// menghapus array
+delete nama[3]
+
+ document.writeln(nama)
